a0eee45369a89843590a15585f310cd27481df32
Benjamin Kampmann <ben@create-build-execute.com>
2015-11-06 13:00:58 +0100

    Reactivate CSS Modules

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,32 +1,31 @@
 import React from "react";
-
-require("./MainNavigation.scss")
+import Styles from "./MainNavigation.scss";
 
 export default class MainNavigation extends React.Component{
   render(){
-    return <header className="navigation" role="banner">
-            <div className="navigation-wrapper">
-              <a href="javascript:void(0)" className="logo">
+    return <header className={Styles.navigation} role="banner">
+            <div className={Styles.navigation_wrapper}>
+              <a href="javascript:void(0)" className={Styles.logo}>
                 <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_1.png" alt="Logo Image" />
               </a>
-              <a href="javascript:void(0)" className="navigation-menu-button" id="js-mobile-menu">MENU</a>
+              <a href="javascript:void(0)" className={Styles.navigation_menu_button} id="js-mobile-menu">MENU</a>
               <nav role="navigation">
-                <ul id="js-navigation-menu" className="navigation-menu show">
-                  <li className="nav-link"><a href="javascript:void(0)">Products</a></li>
-                  <li className="nav-link"><a href="javascript:void(0)">About Us</a></li>
-                  <li className="nav-link"><a href="javascript:void(0)">Contact</a></li>
-                  <li className="nav-link more"><a href="javascript:void(0)">More</a>
-                    <ul className="submenu">
+                <ul id="js-navigation-menu" className={Styles.navigation_menu + " " + Styles.show}>
+                  <li className={Styles.nav_link}><a href="javascript:void(0)">Products</a></li>
+                  <li className={Styles.nav_link}><a href="javascript:void(0)">About Us</a></li>
+                  <li className={Styles.nav_link}><a href="javascript:void(0)">Contact</a></li>
+                  <li className={Styles.nav_link + " " + Styles.more}><a href="javascript:void(0)">More</a>
+                    <ul className={Styles.submenu}>
                       <li><a href="javascript:void(0)">Submenu Item</a></li>
                       <li><a href="javascript:void(0)">Another Item</a></li>
-                      <li className="more"><a href="javascript:void(0)">Item with submenu</a>
-                        <ul className="submenu">
+                      <li className={Styles.more}><a href="javascript:void(0)">Item with submenu</a>
+                        <ul className={Styles.submenu}>
                           <li><a href="javascript:void(0)">Sub-submenu Item</a></li>
                           <li><a href="javascript:void(0)">Another Item</a></li>
                         </ul>
                       </li>
-                      <li className="more"><a href="javascript:void(0)">Another submenu</a>
-                        <ul className="submenu">
+                      <li className={Styles.more}><a href="javascript:void(0)">Another submenu</a>
+                        <ul className={Styles.submenu}>
                           <li><a href="javascript:void(0)">Sub-submenu</a></li>
                           <li><a href="javascript:void(0)">An Item</a></li>
                         </ul>
@@ -35,8 +34,8 @@ export default class MainNavigation extends React.Component{
                   </li>
                 </ul>
               </nav>
-              <div className="navigation-tools">
-                <div className="search-bar">
+              <div className={Styles.navigation_tools}>
+                <div className={Styles.search_bar}>
                   <form role="search">
                     <input type="search" placeholder="Enter Search" />
                     <button type="submit">
